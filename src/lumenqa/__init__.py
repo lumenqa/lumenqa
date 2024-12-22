e3ad06ab9ca1a8a43d77b7f62940e96349f1c98c
LumenQA Team <team@lumenqa.dev>
2024-12-22 14:30:00 +0200

    Add core package structure and version info

---
+++ src/lumenqa/__init__.py
@@ -0,0 +1,9 @@
+"""
+LumenQA - The Light-Speed Automation Framework
+
+A next-generation browser automation framework powered by PyLux and LumenVM.
+"""
+
+from .version import __version__
+
+__all__ = ["__version__"]
---
+++ src/lumenqa/version.py
@@ -0,0 +1,5 @@
+"""Version information for LumenQA"""
+
+__version__ = "0.9.4"
+__lumenvm_version__ = "2.1.3"
+__pylux_version__ = "1.0.0-beta.9"
